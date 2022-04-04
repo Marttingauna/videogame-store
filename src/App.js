f70c21c061019568f001e58cbeb398fa64a11542
Martín <marrtingauna@gmail.com>
2022-04-04 08:54:57 -0300

    Directorio de carpeta, configuracion prettier y modularizando componentes iniciales

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from './components/directory/directory';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const categories = [
+		{
+			_id: 1,
+			title: 'Deportes',
+			imageUrl:
+				'https://assets.spartangeek.com/cc/juegos-de-tenis-para-pc.jpg',
+		},
+		{
+			_id: 2,
+			title: 'Acción y aventura',
+			imageUrl:
+				'https://internetpasoapaso.com/wp-content/uploads/The-Legend-Of-Zelda.jpg',
+		},
+		{
+			_id: 3,
+			title: 'Lucha',
+			imageUrl:
+				'https://pcahora.com/wp-content/uploads/2019/01/mejores-juegos-de-lucha-para-PS4-1.jpg',
+		},
+		{
+			_id: 4,
+			title: 'Arcade',
+			imageUrl:
+				'https://iat.es/wp-content/uploads/2020/12/mejores-juegos-arcade.jpg',
+		},
+		{
+			_id: 5,
+			title: 'Plataforma',
+			imageUrl:
+				'https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/10/Indie-Platform-Games.jpg',
+		},
+	];
+	return <Directory categories={categories} />;
+};
 
 export default App;
