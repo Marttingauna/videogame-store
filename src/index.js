9d634f38ec634758ab4468739be5d58b0002b636
Martín <marrtingauna@gmail.com>
2022-04-06 01:52:13 -0300

    [develop] Cambios REACT 18

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById('root')
+createRoot(document.getElementById('root')).render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
